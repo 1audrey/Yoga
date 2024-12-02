74219c050a92e3e368824685b597f291b25655ba
Audrey-Lescot_waters <audrey_lescot@waters.com>
2024-12-02 14:50:30 +0000

    add buttons around icons for accesbility and change css to scss file

--- yoga-app/src/app/app.module.ts
+++ yoga-app/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { MatIconModule } from '@angular/material/icon';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
